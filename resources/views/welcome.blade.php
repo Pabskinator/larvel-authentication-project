a1f2521e11ee86953f247fa0fcbc8994dc3aeac2
Adrian Pabe <adrianpabs14@gmail.com>
2020-11-27 21:38:30 +0800

    Tweak auth confirmation and middleware

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,6 +45,14 @@
                     </svg>
                 </div>
 
+                <div>
+                    @auth
+                        <h1 style="color:white;">Welcome, {{ auth()->user()->name }}!</h1>
+                    @else
+                        <h1 style="color:white;">Sign in and join us!</h1>
+                    @endauth
+                </div>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
